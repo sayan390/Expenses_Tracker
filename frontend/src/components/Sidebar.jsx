ee55376f7ab314861a51869ff9ee5f1edf0049b8
Sayan Nandi <nandisayan32@gmail.com>
2026-05-29 23:11:58 +0530

    day 7

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -197,7 +197,7 @@ const Sidebar = ({ user, isCollapsed, setIsCollapsed }) => {
                 sidebarStyles.footerLink.base,
                 isCollapsed && sidebarStyles.footerLink.collapsed,
               )}
-              to="https://www.hexagondigitalservices.com/contact"
+              to="https://sayan-nandi-portfolio.netlify.app"
             >
               <HelpCircle size={20} className=" text-gray-500" />
               {!isCollapsed && <span>Support</span>}
@@ -304,7 +304,7 @@ const Sidebar = ({ user, isCollapsed, setIsCollapsed }) => {
                 <div className={sidebarStyles.mobileFooter}>
                   <Link
                     onClick={() => setMobileOpen(false)}
-                    to="https://www.hexagondigitalservices.com/contact"
+                    to="https://sayan-nandi-portfolio.netlify.app"
                     className={sidebarStyles.mobileFooterLink}
                   >
                     <HelpCircle size={20} className=" text-gray-500" />
